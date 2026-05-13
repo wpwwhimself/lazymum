7dcad2377e095f0cc1e0a745ada0fe0fd98ac885
Wojciech Przybyła <contact@wpww.pl>
2026-05-13 22:25:45 +0200

    ⚓ scaffolded roles

---
+++ app/Scaffolds/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Scaffolds;
+
+use App\Scaffolds\Shipyard\Role as ShipyardRole;
+
+class Role extends ShipyardRole
+{
+    protected static function items(): array
+    {
+        return [
+        ];
+    }
+}
